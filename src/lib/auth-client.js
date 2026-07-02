8f1f3956cb1de5162bfb6a07fcec576eae694371
Sujoy <sujaykarmokar86@gmail.com>
2026-07-02 13:09:36 +0600

    final deploy.

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -3,5 +3,5 @@ export const { signIn, signUp, useSession } = createAuthClient()
 
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: "http://localhost:3000"
+    baseURL: "https://ph-conseptual-session-08-next-js-pr.vercel.app"
 })
